df7423acb107d8289bc7013deba7aa1bce95f028
Luka Abramović <lukaabramovic2@gmail.com>
2020-11-14 13:12:07 +0100

    ArraySorter which implements sorting algorithms.
    Heap data structure.

--- Heap.cs
+++ Heap.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
-using System.Net.Http.Headers;
 
 namespace DataStructuresAndAlgorithms
 {
--- IArraySorter.cs
+++ IArraySorter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataStructuresAndAlgorithms
+﻿namespace DataStructuresAndAlgorithms
 {
     interface IArraySorter
     {
